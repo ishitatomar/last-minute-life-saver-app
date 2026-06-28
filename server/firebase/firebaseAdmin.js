a0021bbd1db66bd24e945f60b93b3c6f58cfe163
Ishita Tomar <ishitatomar1992@gmail.com>
2026-06-29 01:25:52 +0530

    fix firebase admin for cloud run env variables

--- server/firebase/firebaseAdmin.js
+++ server/firebase/firebaseAdmin.js
@@ -2,10 +2,12 @@ import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getAuth } from "firebase-admin/auth";
 
-import serviceAccount from "./serviceAccountKey.json" with { type: "json" };
-
 initializeApp({
-  credential: cert(serviceAccount),
+  credential: cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
 });
 
 export const db = getFirestore();
