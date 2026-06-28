ca01b1153ceebb5367918eccc083a00a978f7e89
Ishita Tomar <ishitatomar1992@gmail.com>
2026-06-29 01:44:25 +0530

    fix cloud run port binding issue

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+console.log("App starting...");
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -25,6 +26,6 @@ app.use("/api", aiRoutes);
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on ${PORT}`);
 });
