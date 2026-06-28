fe8d69678831f6b39dd6a7e0ce76f40033a74388
Ishita Tomar <ishitatomar1992@gmail.com>
2026-06-29 02:11:46 +0530

    debug cloud run minimal server

--- server/server.js
+++ server/server.js
@@ -1,31 +1,13 @@
-console.log("App starting...");
-import dotenv from "dotenv";
-dotenv.config();
-
 import express from "express";
-import cors from "cors";
-
-//import "./firebase/firebaseAdmin.js";
-import aiRoutes from "./routes/aiRoutes.js";
-
-console.log(
-  "OpenRouter Key:",
-  process.env.OPENROUTER_API_KEY ? "Loaded ✅" : "Missing ❌"
-);
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-
 app.get("/", (req, res) => {
-  res.send("🚀 Last Minute Life Saver Backend Running");
+  res.send("SERVER WORKING 🚀");
 });
-
-app.use("/api", aiRoutes);
 
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on ${PORT}`);
+  console.log("Server running on", PORT);
 });
