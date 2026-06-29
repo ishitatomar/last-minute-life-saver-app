4892e1e63843e54e388d6f6f5ebe75db0994c539
Ishita Tomar <ishitatomar1992@gmail.com>
2026-06-30 03:40:24 +0530

    Fix backend routes

--- server/server.js
+++ server/server.js
@@ -1,26 +1,31 @@
 console.log("App starting...");
-
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-
-const aiRoutes = require("./routes/aiRoutes");
-
+import dotenv from "dotenv";
 dotenv.config();
+
+import express from "express";
+import cors from "cors";
+
+//import "./firebase/firebaseAdmin.js";
+import aiRoutes from "./routes/aiRoutes.js";
+
+console.log(
+  "OpenRouter Key:",
+  process.env.OPENROUTER_API_KEY ? "Loaded ✅" : "Missing ❌"
+);
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/", aiRoutes);
-
 app.get("/", (req, res) => {
-  res.send("Backend Running");
+  res.send("🚀 Last Minute Life Saver Backend Running");
 });
+
+app.use("/api", aiRoutes);
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT,"0.0.0.0" () => {
   console.log("Server running on", PORT);
 });
